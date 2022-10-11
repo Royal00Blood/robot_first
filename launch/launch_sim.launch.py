2e9ca4e3f85ec462aa7e359e7b934f911863c1a8
Royal00Blood <nazgoool1@gmail.com>
2022-10-11 14:34:32 +0500

    correct file launch_sim

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,19 +1,17 @@
-from http.server import executable
 import os
 
-from ament_index_python.packages import get_packages_share_directory
+from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDiscription
-from launche.launch_description_sources import PythonLaunchDescriptionSource
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
-from symbol import argument
 
 def generate_launch_description():
     package_name = 'robot_first'
-    rsp = IncludeLaunchDiscription(PythonLaunchDescriptionSource([os.path.join(get_packages_share_directory(package_name),'launch','rsp.launch.py')]),launch_arguments={'use_sim_time':'true'}.items())
-    gazebo = IncludeLaunchDiscription(PythonLaunchDescriptionSource([os.path.join(get_packages_share_directory('gazebo_ros'),'launch','gazebo.launch.py')]),)
+    rsp = IncludeLaunchDiscription(PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(package_name),'launch','rsp.launch.py')]),launch_arguments={'use_sim_time':'true'}.items())
+    gazebo = IncludeLaunchDiscription(PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py')]),)
     spawn_entity = Node(package = 'gazebo_ros', executable = 'spawn_entity.py',
                         arguments = ['-topic','robot_description',
                                      '-entity','robot_first'],
