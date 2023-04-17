0cb6e1a099abc34611a1d19ba2afb53fb7933bad
Royal00Blood <Nazgoool1@gmail.com>
2023-04-17 17:45:26 +0400

    speed change

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -12,10 +12,10 @@ def generate_launch_description():
 
     rsp = IncludeLaunchDescription(PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(package_name),'launch','rsp.launch.py')]),launch_arguments={'use_sim_time':'true'}.items())
     
-    # config_file_name = 'params.yaml' 
-    # pkg_path = os.path.join(get_package_share_directory('robot_first'))
-    # config = os.path.join(pkg_path, 'config', config_file_name)
-    gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py')]),launch_arguments={"gui":"false"}.items())#,'extra_gazebo_args': '-ros-args --params-file ' + config
+    config_file_name = 'params.yaml' 
+    pkg_path = os.path.join(get_package_share_directory('robot_first'))
+    config = os.path.join(pkg_path, 'config', config_file_name)
+    gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py')]),launch_arguments={"gui":"False",'extra_gazebo_args': '--ros-args --params-file ' + config}.items())#,'extra_gazebo_args': '-ros-args --params-file ' + config
     
     spawn_entity = Node(package = 'gazebo_ros', 
                         executable = 'spawn_entity.py',
